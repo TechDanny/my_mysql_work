819ce5868a6563f121bcca0fa6b8ee6f506a4982
root <kokidanny15@gmail.com>
2024-01-21 15:10:12 +0300

    Use of functions in mysql

---
+++ Func.sql
@@ -0,0 +1,25 @@
+-- Use mysql functions
+
+-- find the transactions that were takem
+SELECT COUNT(amount) AS count
+FROM transactions;
+
+-- find the maximum amount
+SELECT MAX(amount) AS maximum
+FROM transactions;
+
+-- find the minimum amount
+SELECT MIN(amount) AS minimum
+FROM transactions;
+
+-- calculate the total sum of amount
+SELECT SUM(amount) AS total
+FROM transactions;
+
+-- calculate the average of amount
+SELECT AVG(amount) AS average
+FROM transactions;
+
+-- using concat function on mysql
+SELECT CONCAT(first_name, " ", last_name)
+FROM employees;
